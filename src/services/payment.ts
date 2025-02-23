be5d3235db4250c6ed8b182dd84d8f10dbe2f301
JaysonRonin <jayson_supra@hotmail.com>
2025-02-24 04:10:14 +0800

    chore: Update payment services with try block

--- src/services/payment.ts
+++ src/services/payment.ts
@@ -2,17 +2,28 @@ import { CLIENT_METHOD, CLIENT_SERVICES } from '@/constants';
 import ServiceClient from './ServiceClient';
 
 const payment = {
-  async transferFunds(amount: number, recipient: string) {
-    const response = await ServiceClient.getInstance().authFetch({
-      method: CLIENT_METHOD.POST,
-      service: CLIENT_SERVICES.PAYMENT,
-      url: '/transfer',
-      body: {
-        amount,
+  async transferFunds<T>({
+    amount,
+    recipient,
+  }: {
+    amount: number;
+    recipient: string;
+  }): Promise<T | Error> {
+    try {
+      const body = {
+        amount: JSON.stringify(amount),
         recipient,
-      },
-    });
-    return response;
+      };
+      const response = await ServiceClient.getInstance().authFetch<T>({
+        method: CLIENT_METHOD.POST,
+        service: CLIENT_SERVICES.PAYMENT,
+        url: '/transfer',
+        body,
+      });
+      return response;
+    } catch (error) {
+      throw new Error(error as string);
+    }
   },
 };
 
