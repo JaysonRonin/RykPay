6439a9e4a499df3c81f2ba6eef82c22d161f1719
JaysonRonin <jayson_supra@hotmail.com>
2025-02-24 04:11:16 +0800

    task: Update MainApp cater for ui theme

--- src/app/MainApp.tsx
+++ src/app/MainApp.tsx
@@ -2,6 +2,7 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 import RouteKeys from '@/navigator/RouteKeys';
 import TabNavigator from '@/navigator/TabNavigator';
 import { RouteParamsList } from '@/navigator/typings/types';
+import * as eva from '@eva-design/eva';
 import {
   DarkTheme,
   DefaultTheme,
@@ -9,10 +10,12 @@ import {
   ThemeProvider,
 } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { ApplicationProvider } from '@ui-kitten/components';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 import 'react-native-reanimated';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -23,7 +26,10 @@ export default function MainApp(): React.JSX.Element {
   const [loaded] = useFonts({
     /* eslint-disable @typescript-eslint/no-require-imports */
     /* eslint-disable global-require */
-    Poppins: require('@assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Bold': require('@assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-Medium': require('@assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Regular': require('@assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-SemiBold': require('@assets/fonts/Poppins-SemiBold.ttf'),
   });
 
   useEffect(() => {
@@ -35,13 +41,20 @@ export default function MainApp(): React.JSX.Element {
   const Stack = createStackNavigator<RouteParamsList>();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={RouteKeys.HOME}>
-          <Stack.Screen name={RouteKeys.HOME} component={TabNavigator} />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <KeyboardProvider>
+          <NavigationContainer>
+            <Stack.Navigator
+              screenOptions={{ headerShown: false }}
+              initialRouteName={RouteKeys.HOME}
+            >
+              <Stack.Screen name={RouteKeys.HOME} component={TabNavigator} />
+            </Stack.Navigator>
+          </NavigationContainer>
+          <StatusBar style="auto" />
+        </KeyboardProvider>
+      </ThemeProvider>
+    </ApplicationProvider>
   );
 }
