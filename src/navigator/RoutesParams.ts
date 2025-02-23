1942c8b903a099ec13f06790aba9f87ba1905619
JaysonRonin <jayson_supra@hotmail.com>
2025-02-24 04:08:05 +0800

    task: Created payment failed route

--- src/navigator/RoutesParams.ts
+++ src/navigator/RoutesParams.ts
@@ -4,6 +4,7 @@
 export type TypedRouteParams = PaymentRouteParams;
 
 interface PaymentRouteParams {
+  PAYMENTS_FAILED: { error: Error };
   PAYMENT_CONFIRMATION: { item: string[] };
   RECEIPT_DETAIL: { item: string[] };
 }
